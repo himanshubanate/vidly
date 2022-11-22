ddf2b5e20a33221bf1886516a6d5ae47556711cf
Himanshu <banatehimanshu@gmail.com>
2022-11-22 15:37:45 +0530

    mid2 commit

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import _ from "lodash";
+export default function Pagination(props) {
+  const { itemCounts, pageSize, onPageChange, currentPage } = props;
+  const pagesCount = Math.ceil(itemCounts / pageSize);
 
-export default function pagination(props) {
-  return null;
+  if (pagesCount === 1) return null;
+  const pages = _.range(1, pagesCount + 1);
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,14 +1,35 @@
 import { Fragment, useState } from "react";
 import { getMovies } from "./../services/fakeMovieService";
+import Pagination from "./common/pagination";
+import {Paginate, paginate} from './utils/paginate';
+import Like from "./like";
 function Movies() {
   const [movies, setMovies] = useState(getMovies);
+  const [pageSize] = useState(4);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const allMovies = Paginate(movies, currentPage,pageSize);
 
   const handleDelete = (movie) => {
-    setMovies(movies.filter((m) => m._id != movie._id));
+    setMovies(movies.filter((m) => m._id !== movie._id));
   };
 
   const { length: count } = movies;
   if (count === 0) return <p>There are no movies in the database</p>;
+
+  const handleLike = (movie) => {
+    const index = movies.indexOf(movie);
+    movies[index].liked = !movies[index].liked;
+    setMovies(
+      movies.map((m) => {
+        return m;
+      })
+    );
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   return (
     <Fragment>
@@ -19,31 +40,37 @@ function Movies() {
             <th>Title</th>
             <th>Gnere</th>
             <th>Stock</th>
-            <th>Rate</th> <th></th>
+            <th>Rate</th>
+            <th></th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
-          {movies.map((movie) => (
+          {allMovies.map((movie) => (
             <tr key={movie._id}>
-              <th>{movie.title}</th>
-              <th>{movie.genre.name}</th>
-              <th>{movie.numberInStock}</th>
-              <th>{movie.dailyRentalRate}</th>
-              <th>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <Like liked={movie.liked} onClick={() => handleLike(movie)} />
+              </td>
+              <td>
                 <button
                   onClick={() => handleDelete(movie)}
                   className="btn btn-danger btn-sm"
                 >
                   Delete
                 </button>
-              </th>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
       <Pagination
-        itemsCount={count}
+        itemCounts={count}
         pageSize={pageSize}
+        currentPage={currentPage}
         onPageChange={handlePageChange}
       />
     </Fragment>
